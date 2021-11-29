cfd67bf4befbb09a1c35a72afb25022fdbcf326b
eve <62781493+everllanos46@users.noreply.github.com>
2021-11-29 00:24:54 -0500

    .

--- src/app/pacientes/completar-registro/completar-registro.component.ts
+++ src/app/pacientes/completar-registro/completar-registro.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Paciente } from 'src/app/models/paciente';
+import { Persona } from 'src/app/models/persona';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { PacienteService } from 'src/app/services/paciente.service';
 
 @Component({
   selector: 'app-completar-registro',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./completar-registro.component.css']
 })
 export class CompletarRegistroComponent implements OnInit {
+  paciente: Paciente;
+  usuario: User;
+  persona : Persona;
+  constructor(private pacienteService: PacienteService, private loginService: AuthenticationService) {
+    let currentUser = this.loginService.currentUserValue;
+    this.usuario=currentUser;
+   }
 
-  constructor() { }
-
+  
   ngOnInit(): void {
+    this.getById();
+  }
+
+  getById() {
+    let response;
+    this.pacienteService.getById(this.usuario.idPersona).subscribe((r) => {
+      response = r;
+      console.log(r);
+      this.paciente = response.paciente;
+      this.persona=this.paciente.persona;
+    })
   }
 
 }
